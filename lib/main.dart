0568350539bd913b5229b23c975ed806d794d752
Kalyaan Kanugula <147011800+kalyaan09@users.noreply.github.com>
2024-11-10 21:51:04 -0800

    Project step and cloud aut

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'home_page.dart';
+import 'login_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(RunLiftApp());
+}
+
+class RunLiftApp extends StatelessWidget {
+  const RunLiftApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RunLift',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginScreen(),
+    );
+  }
+}
