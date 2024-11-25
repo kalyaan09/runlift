509c998f066c36361ba84684f2e9b80971ff8446
Pratik Thatte <pratikthatte98@gmail.com>
2024-11-24 21:56:55 -0800

    Signup Screen skeleton UI

---
+++ lib/signup_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  void signupUser() {
+    //Signup logic to be handled in week 4
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Signup")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(controller: nameController, decoration: InputDecoration(labelText: "Name")),
+            TextField(controller: emailController, decoration: InputDecoration(labelText: "Email")),
+            TextField(controller: passwordController, decoration: InputDecoration(labelText: "Password"), obscureText: true),
+            ElevatedButton(onPressed: signupUser, child: Text("Signup")),
+          ],
+        ),
+      ),
+    );
+  }
+}
